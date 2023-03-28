969b491cdef9768618279586574b9802ca6196ff
RiceDaddy321 <0carlitos2@gmail.com>
2023-03-28 17:24:57 -0500

    create main.sh for cluster

---
+++ main.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+### Sets the job's name.
+#SBATCH --job-name=HW2_2
+
+### Sets the job's output file and path.
+#SBATCH --output=result.out.%j
+
+### Sets the job's error output file and path.
+#SBTACH --error=result.err.%j
+
+### Requested number of nodes for this job. Can be a single number or a range.
+#SBATCH -N 2
+
+### Requested partition (group of nodes, i.e. compute, bigmem, gpu, etc.) for the resource allocation.
+#SBATCH -p kimq
+
+### Requested number of gpus
+#SBATCH --gres=gpu:1
+
+### Limit on the total run time of the job allocation.
+#SBATCH --time=10:00:00
+
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+
+echo "Activating TensorFlow-2.6.2 environment"
+source ~/myEnvs/tf_env/bin/activate
+
+echo "Running testTF.py"
+python3 ~/csci4341_hw2-2/hw2-2.py
+
+echo "Deactivating TensorFlow-2.6.2 environment"
+deactivate
+
+echo "Done."
